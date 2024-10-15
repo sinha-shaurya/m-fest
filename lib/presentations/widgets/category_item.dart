5e2ff8f5f9d28f5f1af265a138d6d806a5094c37
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-10-15 17:16:25 +0530

    not required

--- lib/presentations/widgets/category_item.dart
+++ lib/presentations/widgets/category_item.dart
@@ -13,10 +13,12 @@ class CategoryItem extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
       padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [BoxShadow(offset: Offset(0.5, 2),blurRadius: 4, color: Colors.black12)]
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+                offset: Offset(0.5, 2), blurRadius: 4, color: Colors.black12)
+          ]),
       child: Row(
         children: [
           Icon(icon, color: appTheme(context).primaryColor),
