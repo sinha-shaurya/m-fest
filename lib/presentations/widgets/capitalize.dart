03eb36280c352d974f57b385a2fe088bbe0c7e30
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-01-01 17:23:31 +0530

    [created] reusable components for textfield with auto suggestion for city

---
+++ lib/presentations/widgets/capitalize.dart
@@ -0,0 +1,7 @@
+String capitalize(String input) {
+  if (input.trim().isEmpty) return input;
+  return input.split(' ').map((word) {
+    if (word.isEmpty) return word;
+    return word[0].toUpperCase() + word.substring(1).toLowerCase();
+  }).join(' ');
+}
